ec862c281b60b94a01a1308ddf029c39abc5bf10
Daisuke Tsuji <dim0627@gmail.com>
2017-11-05 14:50:30 +0900

    for scroll position.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
@@ -15,21 +15,33 @@ import Home from './Home';
 import Projects from './Projects';
 import NoMatch from './NoMatch';
 
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
 ReactDOM.render(
   <Router>
-    <div>
-      <Helmet>
-        <title>Portfolio - Daisuke Tsuji</title>
-        <link rel="canonical" href={Frontmatters.base_url} />
-      </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/projects" component={Projects} />
-        <Route component={NoMatch} />
-      </Switch>
-      <Footer />
-    </div>
+    <ScrollToTop>
+      <div>
+        <Helmet>
+          <title>Portfolio - Daisuke Tsuji</title>
+          <link rel="canonical" href={Frontmatters.base_url} />
+        </Helmet>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </div>
+    </ScrollToTop>
   </Router>,
   document.getElementById('root'));
 registerServiceWorker();
