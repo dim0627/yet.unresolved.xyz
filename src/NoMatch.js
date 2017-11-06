5a53bc62b5997603ea2c04ac338f50c7375769d1
Daisuke Tsuji <dim0627@gmail.com>
2017-11-06 20:00:26 +0900

    notfound page.

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import {Helmet} from "react-helmet";
+
 import PageHeader from './components/PageHeader';
 
 const NoMatch = () => (
   <div>
-    <div class="l-container narrow">
+    <Helmet>
+      <title>404 Page Not Found - Daisuke Tsuji</title>
+      <meta name="robots" content="noindex,nofollow" />
+    </Helmet>
+    <div className="l-container narrow error">
       <PageHeader title="404" />
     </div>
   </div>
