5b0a93abb574d30831a1c4d9c90b7d686c20c830
Daisuke Tsuji <dim0627@gmail.com>
2020-07-28 07:48:42 +0900

    :ok_hand: profile.

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styles from './Profile.module.scss';
 
 export const Profile = () => (
-  <div>profile</div>
+  <div className={styles.container}>
+    <div className={styles.heading}>
+      <h1 className={styles.name}>Daisuke Tsuji</h1>
+      <div className={styles.description}></div>
+    </div>
+  </div>
 );
