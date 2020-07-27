5f213ef8a231ec76067890eddd885417bc17acb9
Daisuke Tsuji <dim0627@gmail.com>
2020-07-28 08:09:06 +0900

    :ok_hand: thru graphcms.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 // import Head from 'next/head';
 import { Profile } from 'components/Profile';
+import { graphcms } from 'lib/graphcms';
 
-export default function Home() {
+export default function Home({ profile }) {
   return (
     <Profile />
   );
+}
+
+export async function getStaticProps() {
+  const { profiles } = await graphcms.request(
+    `
+      {
+        profiles {
+          fullName
+          description
+          details
+        }
+      }
+    `,
+  );
+
+  console.log(profiles);
+
+  return {
+    props: {
+      profile: [],
+    },
+  };
 }
